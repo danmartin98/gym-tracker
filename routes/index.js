cbb2d37fc771ea7a14eca01f584dacbcf735a436
danmartin98 <danmartin98@icloud.com>
2022-01-23 14:50:03 +0000

    refactored routes folder to es6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Gym tracker" });
 });
 
 module.exports = router;
