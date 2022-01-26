3e5ed40ac341de690e7c816a7f0bf864446a24a1
danmartin98 <danmartin98@icloud.com>
2022-01-26 19:26:23 +0000

    added extra columns to workoutable and decided to remove setTable

--- db/scripts/createWorkoutTable.js
+++ db/scripts/createWorkoutTable.js
@@ -1,6 +1,6 @@
 import query from "../index.js";
 
-const sqlString = `CREATE TABLE IF NOT EXISTS workout (id SERIAL PRIMARY KEY, day TEXT, exercise TEXT, image_url TEXT, video_url TEXT);`;
+const sqlString = `CREATE TABLE IF NOT EXISTS workout (id SERIAL PRIMARY KEY, day TEXT, muscle_group TEXT, exercise TEXT, sets INT, reps_per_set INT, rest_seconds INT, image_url TEXT, video_url TEXT);`;
 
 async function createWorkoutTable() {
   const workoutTable = await query(sqlString);
